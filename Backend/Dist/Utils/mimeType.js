bf99e3c372c0e5541d9712f7a12ac0fd6faa7c62
Leorio21 <102737876+Leorio21@users.noreply.github.com>
2022-08-08 12:59:31 +0200

    Delete Backend/Dist directory

--- Backend/Dist/Utils/mimeType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MIME_TYPES = new Map([
-    ['image/jpeg', 'jpg'],
-    ['image/png', 'png'],
-    ['image/gif', 'gif'],
-    ['image/webp', 'webp']
-]);
-exports.default = MIME_TYPES;
