89275bb8336ffe4cfbe542a6405ff98aef104bca
Leorio21 <Ptitlue@gmail.com>
2022-08-24 15:12:44 +0200

    Feat: Modal
    Affichage des modales avec react-portal

--- frontend/src/Components/Modal/Modal.tsx
+++ frontend/src/Components/Modal/Modal.tsx
@@ -1,3 +1,6 @@
+
+import { createPortal } from 'react-dom';
+
 import classNames from 'classnames';
 import cn from './Modal.module.scss';
 
@@ -8,14 +11,14 @@ interface ModalProps {
 
 const Modal = ({text, onCloseModal}: ModalProps) => {
     
-    return (
+    return createPortal(
         <div className={classNames(cn.overlay)} onClick={onCloseModal}>
             <button className={classNames(cn['modal-button'])} onClick={onCloseModal}>X</button>
             <div className={classNames(cn['modal-card'])}>
                 {text}
             </div>
-        </div>
+        </div>, document.getElementById('modal-root')!
     )
 }
 
-export default Modal
+export default Modal;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import './index.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('app-root') as HTMLElement).render(
         <BrowserRouter>
             <App />
         </BrowserRouter>
