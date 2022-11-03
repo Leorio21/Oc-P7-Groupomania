d6d82257461df6636358e7b35bbc300578a01fcc
Leorio21 <Ptitlue@gmail.com>
2022-11-03 11:18:30 +0100

    Docs: env/readme/seed
    fichier env : ajout de commentaires
    modification du fichier readme
    ajout de données fictives dans la graine prisma

--- Backend/prisma/seed.ts
+++ Backend/prisma/seed.ts
@@ -11,6 +11,105 @@ const userData: Prisma.UserCreateInput[] = [
         avatar: null,
         background: null,
         role: "ADMIN",
+    },
+    {
+        email: "dupond.marc@groupomania.fr",
+        firstName: "marc",
+        lastName: "dupond",
+        password: "$2b$12$12ALM8OaQDpJfup0hFOTY.cSvbXkuPktnJUC01PHoOeJ6M5p72Z3O",
+        avatar: null,
+        background: null,
+        role: "USER",
+        post: {
+            create: [
+                {
+                    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices felis nec pretium iaculis. Sed sit amet ante justo. Sed eu quam a elit condimentum ultricies. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus at arcu eu sapien fermentum accumsan a at ipsum. Sed rhoncus ornare velit vel dapibus. Aliquam hendrerit mauris ut dui rhoncus rutrum. Aliquam quis cursus justo.",
+                    image: null,
+                    updatedBy: null,
+                }
+            ],
+        },
+        comment: {
+            create: [
+                {
+                    content: "Duis nulla elit, aliquam tristique gravida at, rutrum a nunc.",
+                    postId: 1,
+                }
+            ],
+        },
+        like: {
+            create: [
+                {
+                    postId: 1,
+                }
+            ],
+        },
+    },
+    {
+        email: "noel.pere@groupomania.fr",
+        firstName: "pere",
+        lastName: "noel",
+        password: "$2b$12$12ALM8OaQDpJfup0hFOTY.cSvbXkuPktnJUC01PHoOeJ6M5p72Z3O",
+        avatar: null,
+        background: null,
+        role: "USER",
+        post: {
+            create: [
+                {
+                    content: "Vivamus vitae tincidunt ipsum. Maecenas congue tincidunt faucibus. Curabitur ut sollicitudin enim, nec pulvinar justo. Aliquam porta ultricies sodales. Duis iaculis viverra volutpat. Nunc semper nulla dui, at porta arcu interdum non. Pellentesque nulla quam, consequat eu arcu at, ullamcorper auctor lectus. Proin sagittis pretium tincidunt. Integer condimentum gravida dictum.",
+                    image: null,
+                    updatedBy: null,
+                }
+            ],
+        },
+        comment: {
+            create: [
+                {
+                    content: "Donec risus est, faucibus sed blandit sed, hendrerit ut ex.",
+                    postId: 1,
+                }
+            ],
+        },
+        like: {
+            create: [
+                {
+                    postId: 1,
+                }
+            ],
+        },
+    },
+    {
+        email: "smith.john@groupomania.fr",
+        firstName: "john",
+        lastName: "smith",
+        password: "$2b$12$12ALM8OaQDpJfup0hFOTY.cSvbXkuPktnJUC01PHoOeJ6M5p72Z3O",
+        avatar: null,
+        background: null,
+        role: "USER",
+        post: {
+            create: [
+                {
+                    content: "Vestibulum et arcu orci. Integer aliquam quis lacus id sollicitudin. Nulla facilisi. Integer maximus nulla libero, at malesuada risus sagittis vitae. Vestibulum lacus eros, egestas quis ex at, vehicula posuere libero.",
+                    image: null,
+                    updatedBy: null,
+                }
+            ],
+        },
+        comment: {
+            create: [
+                {
+                    content: "Nullam eu ex pulvinar, placerat orci vel, condimentum sem.",
+                    postId: 2,
+                }
+            ],
+        },
+        like: {
+            create: [
+                {
+                    postId: 1,
+                }
+            ],
+        },
     }
 ];
 
--- readme.md
+++ readme.md
@@ -3,21 +3,47 @@ Projet 7 - Parcours Dev Web OpenClassroom - Groupomania
 Dévellopper avec React (vitejs) - typescript - axios - slq - node - express - prisma
 
 Après avoir cloner le projet, effectuer les actions suivantes :
+
 ce projet nécéssite d'avoir nodeJS installé sur son ordinateur
 
+
 /********** BACKEND **********/
-dans votre base de données sql créer une bdd : groupomania
+
 dans le dossier backend :
-installer les dependances : npm i
-compiler les fichier typescript : npx tsc
-modifier le fichier .env.sample pour inserer vos données puis le renommer en .env
-créer les tables dans la bdd : npx prisma migrate dev (des données de test sont automatiquement générées)
-démarrer le serveur back: cd dist
-                      npx nodemon server
+
+1/ installer les dependances :
+
+npm i
+
+2/ compiler les fichier typescript :
+
+npx tsc
+
+3/ modifier le fichier .env.sample pour inserer vos données puis le renommer en .env
+
+4/ Si vous possédez le dump SQL de la BDD importez le dans votre BDD et passer à l'étape 5 demarrer le serveur back
+
+Sinon dans votre base de données sql créer une bdd : groupomania
+
+pour créer les tables dans la bdd : npx prisma migrate dev (des données de test sont automatiquement générées)
+
+En cas d'erreur lors de la génération des données (tables créer mais pas de données) lancer la commande suivante : npx prisma migrate reset
+
+5/ démarrer le serveur back:
+
+cd dist
+
+npx nodemon server
 
 
 /********** FRONTEND **********/
+
 dans le dossier frontend :
-installer les dependances : npm i
-modifier le fichier .env.sample pour inserer vos données puis le renommer en .env
-demarrer le serveur front : npm run dev
+
+1/ installer les dependances : npm i
+
+2/ modifier le fichier .env.sample pour inserer vos données puis le renommer en .env
+
+3/ demarrer le serveur front :
+
+npm run dev
